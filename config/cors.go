cb253d069c51eba918a8047762ac700b922590f0
Indra Riksa Herlambang <indrariksa@gmail.com>
2025-07-06 14:35:11 +0700

    Add additional allowed origin for CORS configuration

--- config/cors.go
+++ config/cors.go
@@ -6,6 +6,7 @@ var allowedOrigins = []string{
 	"http://localhost:5174/",
 	"http://localhost:5175/",
 	"http://127.0.0.1:8088/",
+	"https://fanciful-meerkat-d8b20e.netlify.app/",
 }
 
 func GetAllowedOrigins() []string {
